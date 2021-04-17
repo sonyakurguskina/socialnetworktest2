d7bc50e122ce87833ae6a57beef5a44aab19fd39
Sonya Kurguskina <sonyakurguskinaa@gmail.com>
2021-04-17 12:08:15 +0300

    final

--- src/itis/socialtest/AnalyticsServiceImpl.java
+++ src/itis/socialtest/AnalyticsServiceImpl.java
@@ -2,12 +2,15 @@ package itis.socialtest;
 
 import itis.socialtest.entities.Post;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class AnalyticsServiceImpl implements AnalyticsService {
     @Override
     public List<Post> findPostsByDate(List<Post> posts, String date) {
-        return null;
+
+        return posts.stream().filter(current -> current.getDate().equals(date)).collect(Collectors.toList());
     }
 
     @Override
@@ -17,11 +20,12 @@ public class AnalyticsServiceImpl implements AnalyticsService {
 
     @Override
     public Boolean checkPostsThatContainsSearchString(List<Post> posts, String searchString) {
-        return null;
+
+        return posts.stream().anyMatch(current -> current.getContent().contains(searchString));
     }
 
     @Override
     public List<Post> findAllPostsByAuthorNickname(List<Post> posts, String nick){
-        return null;
+        return posts.stream().filter(current -> current.getAuthor().getNickname().equals(nick)).collect(Collectors.toList());
     }
 }
--- src/itis/socialtest/MainClass.java
+++ src/itis/socialtest/MainClass.java
@@ -1,8 +1,15 @@
 package itis.socialtest;
 
-
+import itis.socialtest.entities.Author;
 import itis.socialtest.entities.Post;
 
+import java.io.*;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -31,16 +38,81 @@ import java.util.List;
 
 public class MainClass {
 
+    private static final String COMMA_DELIMITER = ",";
     private List<Post> allPosts;
 
     private AnalyticsService analyticsService = new AnalyticsServiceImpl();
 
     public static void main(String[] args) {
-        new MainClass().run("", "");
+        new MainClass().run("/resources/PostDatabase.csv", "/resources/Authors.csv");
     }
 
     private void run(String postsSourcePath, String authorsSourcePath) {
+        try {
+            BufferedReader authorReader = new BufferedReader(new FileReader(authorsSourcePath));
+
+            long userId = 0;
+            String username;
+            String birthday;
+
+            ArrayList<String[]> arrayList = new ArrayList<>();
+
+            //Author parsing
+            String line;
+            while ((line = authorReader.readLine()) != null) {
+                arrayList.add(line.split("\n"));
+            }
+            authorReader.close();
+
+            ArrayList<Author> allAuthors = new ArrayList<>();
+            for (String[] data : arrayList) {
+                String[] currentLine = data[0].split(",");
+                userId = Long.parseLong(currentLine[0]);
+                username = currentLine[1];
+                birthday = currentLine[2];
+                Author author = new Author(userId, username, birthday);
+                allAuthors.add(author);
+            }
+
+            //Post parsing
+            BufferedReader postsReader = new BufferedReader(new FileReader(postsSourcePath));
+            while ((line = postsReader.readLine()) != null) {
+                arrayList.add(line.split("\n"));
+            }
+            postsReader.close();
+
+            arrayList.clear();
+
+            int authorId = 0;
+            StringBuilder content = new StringBuilder();
+            long likesCounter;
+            String date;
 
 
+            for (String[] data : arrayList) {
+                String[] currentLine = data[0].split(",");
+
+                authorId = Integer.parseInt(currentLine[0]);
+                likesCounter = Long.parseLong(currentLine[1]);
+                date = currentLine[2];
+
+                for (int j = 3; j < currentLine.length; j++) {
+                    content.append(currentLine[j]);
+                }
+
+                for (Author author : allAuthors) {
+                    if (author.getId() == authorId) {
+                        Post post = new Post(date, content.toString(), likesCounter, author);
+                        allPosts.add(post);
+                    }
+                }
+            }
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
